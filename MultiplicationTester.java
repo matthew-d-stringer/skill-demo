7331bc82b3c71cc3b6cefd69cae829fbe77682d6
mdsflyboy <mdsflyboy@gmail.com>
2022-04-30 21:56:37 -0700

    Adding unit test for Multiplication

---
+++ MultiplicationTester.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MultiplicationTester {
+    @Test
+    public void multiplyTester() {
+        assertEquals(1 * 3, Muliplication.multiply(1, 3));
+    }
+}
